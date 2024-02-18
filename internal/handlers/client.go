d69ef5406f87ca70175a5dcaf7ff6eb0362a61ea
Dmitriy Groschovskiy <groschovskiy@gmail.com>
2024-02-19 01:06:54 +0200

    Add JSONIpInformationHandler to handle JSON response for IP address

--- internal/handlers/client.go
+++ internal/handlers/client.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func RAWIpInformationHandler(w http.ResponseWriter, r *http.Request) {
 	IPAddress := r.Header.Get("X-Real-Ip")
+	w.Write([]byte(IPAddress))
 
 	if IPAddress == "" {
 		IPAddress = r.Header.Get("X-Forwarded-For")
@@ -15,12 +16,19 @@ func RAWIpInformationHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(IPAddress))
 	}
 
-	w.Write([]byte(IPAddress))
 }
 
-// func JSONIpInformationHandler(w http.ResponseWriter, r *http.Request) {
-
-// }
+func JSONIpInformationHandler(w http.ResponseWriter, r *http.Request) {
+	ip_addr := r.Header.Get("X-Real-Ip")
+	if ip_addr == "" {
+		ip_addr = r.Header.Get("X-Forwarded-For")
+	}
+	if ip_addr == "" {
+		ip_addr = r.RemoteAddr
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"ip_address":"` + ip_addr + `"}`))
+}
 
 // func RAWGeoInformationHandler(w http.ResponseWriter, r *http.Request) {
 
